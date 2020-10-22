3dcafe04dbbb0d749a5ae403271170d86274a5fb
Tomas Granda <tomas.granda@outlook.com>
2020-10-21 23:09:36 -0300

    Enemy Controller

---
+++ Assets/Code/Enemy/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 0.5f;
+
+    private CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        characterController.Move(Vector2.down * Time.deltaTime * speed);
+    }
+}
