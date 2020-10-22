0c4a3afe07dd55ec2ef288414ab6bc680a719724
Tomas Granda <tomas.granda@outlook.com>
2020-10-22 02:15:21 -0300

    Added EnemyGenerator script

---
+++ Assets/Code/Enemy/EnemyGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public float spawnInterval = 1;
+
+    public GameObject enemy;
+
+    void Start()
+    {
+        InitSpawn();
+    }
+
+    void Spawn()
+    {
+        GameObject.Instantiate(enemy, transform.position, transform.rotation);
+    }
+
+    void InitSpawn()
+    {
+        IEnumerator myWaitCoroutine()
+        {
+            yield return new WaitForSeconds(spawnInterval);
+
+            Spawn();
+
+            StartCoroutine(myWaitCoroutine());
+        }
+        StartCoroutine(myWaitCoroutine());
+    }
+}
