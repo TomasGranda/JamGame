0486badd0aa0a001ea9406e1c272a8ac8d32aec9
Tomas Granda <tomas.granda@outlook.com>
2020-10-21 23:39:16 -0300

    Added Enemy Shoot Code

--- Assets/Code/Enemy/EnemyController.cs
+++ Assets/Code/Enemy/EnemyController.cs
@@ -6,15 +6,40 @@ public class EnemyController : MonoBehaviour
 {
     public float speed = 0.5f;
 
+    public float shootInterval = 1;
+
+    public GameObject enemyShoot;
+
     private CharacterController characterController;
+
+    private float time;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        InitShoot();
     }
 
     void Update()
     {
         characterController.Move(Vector2.down * Time.deltaTime * speed);
+    }
+
+    void Shoot()
+    {
+        GameObject.Instantiate(enemyShoot, transform.position, transform.rotation);
+    }
+
+    void InitShoot()
+    {
+        IEnumerator myWaitCoroutine()
+        {
+            yield return new WaitForSeconds(shootInterval);
+
+            Shoot();
+
+            StartCoroutine(myWaitCoroutine());
+        }
+        StartCoroutine(myWaitCoroutine());
     }
 }
